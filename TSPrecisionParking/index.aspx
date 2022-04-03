57935a5f66643afcab183935aa464523a965ae1c
jackson <jackson@heehoposting.xyz>
2022-04-03 15:09:04 -0400

    Added a function to add a new sensor and launch it with a precise timestamp.

---
+++ TSPrecisionParking/index.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="TSPrecisionParking.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="btnNew" runat="server" Text="New Sensor"  OnClick="btnNew_Click"/><br />
+            <asp:DropDownList ID="ddlSensors" runat="server" DataSourceID="sqlsrcListSensors" DataTextField="SensorID" DataValueField="SensorID" AutoPostBack="true"></asp:DropDownList><br />
+            <asp:Button ID="btnLaunch" runat="server" Text="Launch Sensor" OnClick="btnLaunch_Click" /><br />
+        </div>
+        <asp:SqlDataSource ID="sqlsrcListSensors" runat="server"
+        ConnectionString="<%$ ConnectionStrings:TSPP %>"
+        SelectCommand="SELECT SensorID FROM Sensor;"></asp:SqlDataSource>
+    </form>
+</body>
+</html>
---
+++ TSPrecisionParking/index.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+// SQL imports
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+using System.Web.UI.HtmlControls;
+
+namespace TSPrecisionParking
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnNew_Click(object sender, EventArgs e)
+        {
+            // update command, helper method used to determine table to be used
+            String sqlQuery = "INSERT INTO Sensor (LocationID) VALUES (NULL);";
+
+
+            // def connections, create commands, insert parameter, and execute query
+            SqlConnection sqlConnect = new SqlConnection(WebConfigurationManager.ConnectionStrings["TSPP"].ConnectionString);
+            SqlCommand sqlCommand = new SqlCommand(sqlQuery, sqlConnect);
+            sqlConnect.Open();
+            sqlCommand.ExecuteScalar();
+            sqlConnect.Close();
+        }
+
+        protected void btnLaunch_Click(object sender, EventArgs e)
+        {
+            
+            String sqlQuery = "UPDATE Sensor SET StartTime = '" +  DateTime.Now + "' WHERE SensorID = '" + ddlSensors.SelectedValue + "';";
+
+
+            // def connections, create commands, insert parameter, and execute query
+            SqlConnection sqlConnect = new SqlConnection(WebConfigurationManager.ConnectionStrings["TSPP"].ConnectionString);
+            SqlCommand sqlCommand = new SqlCommand(sqlQuery, sqlConnect);
+            sqlConnect.Open();
+            sqlCommand.ExecuteScalar();
+            sqlConnect.Close();
+
+        }
+
+
+
+    }
+}
